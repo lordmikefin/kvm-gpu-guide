7311fcb4ac5d42e7969a58b702e7fab01a87866d
Lord Mike <lordmike@iki.fi>
2018-08-12 12:38:32 +0300

    Initial init.bat file :)

---
+++ guest/windows/init.bat
@@ -0,0 +1,15 @@
+::C:\Windows\SysWOW64\cmd.exe
+
+:: Copyright (c) 2017, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+:: All rights reserved.
+:: 
+:: License of this script file:
+::   BSD 2-Clause License
+:: 
+:: License is available online:
+::   https://github.com/lordmikefin/kvm-gpu-guide/blob/master/LICENSE.rst
+:: 
+:: Latest version of this script file:
+::  https://github.com/lordmikefin/kvm-gpu-guide/blob/master/guest/windows/init.bat
+
+
