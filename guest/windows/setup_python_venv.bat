8269491c54043eda94c128fb33b85805223cb511
Lord Mike <lordmike@iki.fi>
2018-08-12 18:53:52 +0300

    Really setup venv.

--- guest/windows/setup_python_venv.bat
+++ guest/windows/setup_python_venv.bat
@@ -16,7 +16,7 @@
 :: This script will setup Python virtual environment for my scripts :)
 
 
-SET CURRENT_SCRIPT_VER=0.0.1
+SET CURRENT_SCRIPT_VER=0.0.2
 SET CURRENT_SCRIPT_DATE=2018-08-12
 echo CURRENT_SCRIPT_VER: %CURRENT_SCRIPT_VER% (%CURRENT_SCRIPT_DATE%)
 echo.
@@ -46,57 +46,77 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: TODO: list all virtual environments
 
+
+echo.
 echo I will try to use virtual environment 'venv-lm-scripts'.
 echo All my python scripts will use this environment.
+echo.
 ::call %USERPROFILE%\Envs\venv-lm-scripts\Scripts\activate.bat
-call workon venv-lm-scripts
+call workon venv-lm-scripts10
+SET SUCCESS=0
 if %errorlevel% neq 0 ( 
-	pause
-	::if %errorlevel% neq 0 ( echo "Still error" )
-	::cmd /c "exit /b 0"
-	::if %errorlevel% neq 0 ( echo "Still error" )
-	echo.
-	echo.
-	echo I will create a new virtual environment 'venv-lm-scripts'
-	::echo "Press enter to contitue."
-	echo.
-	pause
-	echo.
-	call mkvirtualenv --python=C:\Python\Python34\python.exe venv-lm-scripts
-	echo.
+	SET SUCCESS=0
+	::echo errorlevel: %errorlevel%
 	
-	echo Python 3.7 should be installed into C:\Python\Python34\
-	echo https://www.python.org/ftp/python/3.4.4/python-3.4.4.amd64.msi
+	:: clear the error level
+	::cmd /c exit 0
+	call cmd /c "exit /b 0"
+	::VERIFY > nul
+	::echo errorlevel: %errorlevel%
 	echo.
-	echo Run this script again.
-	call deactivate
-	exit /b 0
-	
-	:: NOTE(Mikko): I can not clear the %errorlevel%
-	::              No matter what 'mkvirtualenv' returns code is still in error mode ???
-	pause
-	echo.
-	if %errorlevel% neq 0 ( 
-		::echo "This script will now exit. Just becaus. TODO: fix this."
-		echo Python 3.4 should be installed into C:\Python\Python34\
-		echo https://www.python.org/ftp/python/3.4.4/python-3.4.4.amd64.msi
-		exit /b %errorlevel% 
-	) else (  
-		call workon venv-lm-scripts
-		if %errorlevel% neq 0 ( 
-			exit /b %errorlevel% 
-		) else ( 
-			echo Now workon venv-lm-scripts
-		)
-	)
+	echo Virtual environment 'venv-lm-scripts' not found (as shuld not).
+	echo Next I will create the venv.
 	echo.
 	pause
 	
 ) else ( 
+	SET SUCCESS=1
+	echo.
+	echo Virtual environment 'venv-lm-scripts' already exists.
 	echo Now workon venv-lm-scripts
+	echo.
 )
 
+:: clear the error level
+::(call)
+:: (call )
+::VERIFY > nul
+::set errorlevel=
+::echo SUCCESS: %SUCCESS%
+::echo errorlevel: %errorlevel%
+::exit /b 0
 
+if %SUCCESS% neq 1 ( 
+	echo errorlevel: %errorlevel%
+	echo I will create a new virtual environment 'venv-lm-scripts'
+	echo  $ mkvirtualenv --python=C:\Python\Python37\python.exe venv-lm-scripts
+	echo.
+	
+	call mkvirtualenv --python=D:\apps\python\python37\python.exe venv-lm-scripts10
+	:: Batch will not catch the error within if statement?!?!?! WTF!
+	::if %errorlevel% neq 0 ( 
+	::	echo errorlevel: %errorlevel%
+	::	pause
+	::	exit /b %errorlevel%
+	::)
+)
+
+echo errorlevel: %errorlevel%
+
+:: Handle possible errors from 'call mkvirtualenv...'
+if %errorlevel% neq 0 ( 
+	echo errorlevel: %errorlevel%
+	echo.
+	echo Python 3.7 should be installed into D:\apps\python\python37\
+	echo https://www.python.org/ftp/python/3.7.0/python-3.7.0-amd64.exe
+	echo.
+	echo Please run init.bat first. It should install Python in my way.
+	echo.
+	pause
+	call exit /b %errorlevel%
+)
+
+echo.
 echo Exit from virtual environment 'venv-lm-scripts'.
 echo  $ deactivate
 echo.
@@ -106,4 +126,5 @@ call deactivate
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo.
 
+echo Script DONE.
 pause
