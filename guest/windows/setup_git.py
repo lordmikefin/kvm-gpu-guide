e0829ffa1cc078ba96173a0d7fbd1756fcfbc042
Lord Mike <lordmike@iki.fi>
2018-08-12 21:24:56 +0300

    Pyhton docstring and argument handling testings

--- guest/windows/setup_git.py
+++ guest/windows/setup_git.py
@@ -11,7 +11,7 @@
        BSD 2-Clause License
 
     License is available online:
-       https://github.com/lordmikefin/kvm-gpu-guide/blob/master/LICENSE.rst
+      https://github.com/lordmikefin/kvm-gpu-guide/blob/master/LICENSE.rst
 
     Latest version of this script file:
       https://github.com/lordmikefin/kvm-gpu-guide/blob/master/guest/windows/setup_git.py
@@ -20,9 +20,42 @@
     :copyright: (c) 2018, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
     :license: BSD 2-Clause License
 
+    :todo: Verity that folder D:\apps\ exists
+    :todo: Install Notepad++ first.
 """
 
+# https://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html
+# http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+__license__ = "BSD 2-Clause License"
+#__revision__ = " $Id: actor.py 1586 2009-01-30 15:56:25Z cokelaer $ "
+__revision__ = "setup_git.py  v0.0.2 (2018-08-12)"
+#__docformat__ = 'reStructuredText'
+
 import os
+
+# https://stackoverflow.com/questions/7791574/how-can-i-print-a-python-files-docstring-when-executing-it
+
+if __name__ == '__main__':
+    #print('main')
+    #print(__doc__)
+    print(__license__)
+    print(__revision__)
+    #print(__docformat__)
+    #import doctest
+    #print(str(doctest.testmod()))
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+    # Add your arguments here
+    '''
+    parser.add_argument("-f", "--file", dest="myFilenameVariable",
+                        required=True,
+                        help="write report to FILE", metavar="FILE")
+    '''
+    args = parser.parse_args()
+    #print(args.myFilenameVariable)
+
+
 
 # Install git
 
@@ -32,7 +65,7 @@ import os
 
 # https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
 # TODO: download file from web
-# 		Werify downloaded file is what we were downloading.
+#         Werify downloaded file is what we were downloading.
 
 installer_file = "Git-2.18.0-64-bit"
 installer_path = "W:/"
@@ -40,9 +73,11 @@ installer_file_fullname = str(installer_path) + str(installer_file)
 
 print(str(installer_file_fullname))
 
-print('PATH : ' + str(os.environ.get('PATH')))
+#print('PATH : ' + str(os.environ.get('PATH')))
 # echo %VIRTUAL_ENV%
 print('VIRTUAL_ENV : ' + str(os.environ.get('VIRTUAL_ENV')))
+print('')
+
 
 # https://stackoverflow.com/questions/14894993/running-windows-shell-commands-with-python
 
@@ -59,6 +94,9 @@ import os
 os.system('dir C:\\')
 print('')
 '''
+
+# NOTE: This script assumes that folder D:\apps\ exists
+# TODO: Verity that folder D:\apps\ exists
 
 # NOTE: I like to use Notepad++ as default editor.
 # TODO: Install Notepad++ first.
