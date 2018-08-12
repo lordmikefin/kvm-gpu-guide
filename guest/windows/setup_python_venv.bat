67ac678b1780514d696c46d313fa145e0afe2578
Lord Mike <lordmike@iki.fi>
2018-08-12 19:18:24 +0300

    Install required modules into venv.

--- guest/windows/setup_python_venv.bat
+++ guest/windows/setup_python_venv.bat
@@ -16,7 +16,8 @@
 :: This script will setup Python virtual environment for my scripts :)
 
 
-SET CURRENT_SCRIPT_VER=0.0.2
+SET CURRENT_SCRIPT=setup_python_venv.bat
+SET CURRENT_SCRIPT_VER=0.0.5
 SET CURRENT_SCRIPT_DATE=2018-08-12
 echo CURRENT_SCRIPT_VER: %CURRENT_SCRIPT_VER% (%CURRENT_SCRIPT_DATE%)
 echo.
@@ -52,19 +53,18 @@ echo I will try to use virtual environment 'venv-lm-scripts'.
 echo All my python scripts will use this environment.
 echo.
 ::call %USERPROFILE%\Envs\venv-lm-scripts\Scripts\activate.bat
-call workon venv-lm-scripts10
+call workon venv-lm-scripts
 SET SUCCESS=0
 if %errorlevel% neq 0 ( 
 	SET SUCCESS=0
-	::echo errorlevel: %errorlevel%
 	
 	:: clear the error level
 	::cmd /c exit 0
-	call cmd /c "exit /b 0"
+	::call cmd /c "exit /b 0"
 	::VERIFY > nul
 	::echo errorlevel: %errorlevel%
 	echo.
-	echo Virtual environment 'venv-lm-scripts' not found (as shuld not).
+	echo Virtual environment 'venv-lm-scripts' not found.
 	echo Next I will create the venv.
 	echo.
 	pause
@@ -77,12 +77,13 @@ if %errorlevel% neq 0 (
 	echo.
 )
 
+
 :: clear the error level
 ::(call)
 :: (call )
 ::VERIFY > nul
 ::set errorlevel=
-::echo SUCCESS: %SUCCESS%
+echo SUCCESS: %SUCCESS%
 ::echo errorlevel: %errorlevel%
 ::exit /b 0
 
@@ -92,7 +93,7 @@ if %SUCCESS% neq 1 (
 	echo  $ mkvirtualenv --python=C:\Python\Python37\python.exe venv-lm-scripts
 	echo.
 	
-	call mkvirtualenv --python=D:\apps\python\python37\python.exe venv-lm-scripts10
+	call mkvirtualenv --python=D:\apps\python\python37\python.exe venv-lm-scripts
 	:: Batch will not catch the error within if statement?!?!?! WTF!
 	::if %errorlevel% neq 0 ( 
 	::	echo errorlevel: %errorlevel%
@@ -116,6 +117,33 @@ if %errorlevel% neq 0 (
 	call exit /b %errorlevel%
 )
 
+echo. 
+echo (venv-lm-scripts) $ pip3 list --format=columns
+echo.
+call pip3 list --format=columns
+echo.
+echo.
+
+
+echo. 
+echo Install all needed Python modules into venv.
+echo (venv-lm-scripts) $ pip3 install -U -r requirements.txt
+echo.
+call pip3 install -U -r requirements.txt
+echo.
+echo.
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo.
+echo List all outdated modules.
+echo (venv-lm-scripts) $ pip3 list -o --format=columns 
+echo.
+call pip3 list -o --format=columns 
+echo.
+echo.
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+
 echo.
 echo Exit from virtual environment 'venv-lm-scripts'.
 echo  $ deactivate
@@ -126,5 +154,7 @@ call deactivate
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo.
 
-echo Script DONE.
+
+echo End of script %CURRENT_SCRIPT%
+::echo Script DONE.
 pause
