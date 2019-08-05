b0fbf08630f966a67e492902be9029bd707b8c14
Lord Mike <lordmike@iki.fi>
2019-08-05 21:58:54 +0300

    Harmonize the linux scripts.

--- guest/linux/ubuntu-mate_16_04_1_LTS_dev_cuda.sh
+++ guest/linux/ubuntu-mate_16_04_1_LTS_dev_cuda.sh
@@ -18,17 +18,26 @@
 
 
 unset CURRENT_SCRIPT_VER CURRENT_SCRIPT_DATE
-CURRENT_SCRIPT_VER="0.0.1"
-CURRENT_SCRIPT_DATE="2018-08-05"
+CURRENT_SCRIPT_VER="0.0.2"
+CURRENT_SCRIPT_DATE="2019-08-05"
 echo "CURRENT_SCRIPT_VER: ${CURRENT_SCRIPT_VER} (${CURRENT_SCRIPT_DATE})"
+
+
 
 
 # NOTE to myself: How to get absolute path of file. 
 #   ( https://stackoverflow.com/questions/17577093/how-do-i-get-the-absolute-directory-of-a-file-in-bash )
 
 unset CURRENT_SCRIPT CURRENT_SCRIPT_REALPATH CURRENT_SCRIPT_DIR WORK_DIR
+
 #CURRENT_SCRIPT_REALPATH="$(realpath ${0})"
-CURRENT_SCRIPT_REALPATH="$(realpath ${BASH_SOURCE[0]})"
+#CURRENT_SCRIPT_REALPATH="$(realpath ${BASH_SOURCE[0]})"
+# https://github.com/dgibbs64/SteamCMD-Commands-List
+# https://github.com/dgibbs64/SteamCMD-Commands-List/blob/master/steamcmd_commands.sh
+CURRENT_SCRIPT_REALPATH="$(readlink -f "${BASH_SOURCE[0]}")"
+# NOTE: Should I always use "--canonicalize" ??
+# TODO: Test how "--canonicalize" work if handling broken link.
+
 CURRENT_SCRIPT="$(basename ${CURRENT_SCRIPT_REALPATH})"
 CURRENT_SCRIPT_DIR="$(dirname ${CURRENT_SCRIPT_REALPATH})"
 WORK_DIR="${PWD}"
@@ -131,6 +140,10 @@ fi
 
 
 
+
+
+
+
 # Check if KVM_WORKSPACE variable is set.
 
 # if not ask user if she/he wants to continue using default path:
@@ -149,6 +162,8 @@ lm_check_KVM_WORKSPACE
 
 
 
+
+
 # OVMF binary file. Do _NOT_ over write.
 OVMF_CODE="/usr/share/OVMF/OVMF_CODE.fd"
 KVM_WORKSPACE_VM_UBUNTU="${KVM_WORKSPACE}/vm/ubuntu16_04_dev_cuda"
@@ -156,15 +171,22 @@ OVMF_VARS_UBUNTU="${KVM_WORKSPACE_VM_UBUNTU}/ubuntu16_04_dev_cuda_VARS.fd"
 VM_DISK_UBUNTU="${KVM_WORKSPACE_VM_UBUNTU}/ubuntu16_04_dev_cuda.qcow2"
 KVM_WORKSPACE_SOFTWARE="${KVM_WORKSPACE}/software"
 
+
+
+
+
+
+
 unset INPUT
 lm_read_to_INPUT "Do you wanna use folder ${KVM_WORKSPACE_VM_UBUNTU} for virtual machine?"
 case "${INPUT}" in
-	"YES" ) ;;
-	"NO" ) exit 1 ;;
+	"YES" )
+		INPUT="YES" ;;
+	"NO" )
+		exit 1 ;;
 	"FAILED" | * )
-		lm_failure_message; exit 1 ;;
+		lm_failure ;;
 esac
-
 
 unset INPUT
 lm_read_to_INPUT "Do you wanna share folder ${KVM_WORKSPACE_SOFTWARE} with virtual machine?"
@@ -184,21 +206,34 @@ case "${INPUT}" in
 esac
 
 
+
+
+
+
 # OVMF file for vm. UEFI boot.
 if [[ ! -f "${OVMF_VARS_UBUNTU}" ]]; then
-	#echo -e "\n File ${OVMF_VARS_UBUNTU} does not exists.\n"
-	#lm_failure_message
 	lm_failure_message "${BASH_SOURCE[0]}" "${LINENO}" "File ${OVMF_VARS_UBUNTU} does not exists."
 	exit 1
 fi
 
-
 # Create Ubuntu vm virtual disk.
 if [[ ! -f "${VM_DISK_UBUNTU}" ]]; then
-	#echo -e "\n File ${VM_DISK_UBUNTU} does not exists.\n"
 	lm_failure_message "${BASH_SOURCE[0]}" "${LINENO}" "File ${VM_DISK_UBUNTU} does not exists."
 	exit 1
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -208,11 +243,8 @@ echo ""
 
 
 
-
-
 # TODO: How to get GPU bus address? Ask from user?
 #		Set to common file. Load from there.
-
 
 
 VM_GPUS_CONF_FILE="$(realpath "${CURRENT_SCRIPT_DIR}/../../host/ubuntu-mate_16_04_1_LTS/vm_GPUs.conf")"
@@ -410,19 +442,69 @@ PAR="${PAR} -boot menu=on"
 #PAR="${PAR} -vga qxl"
 #PAR="${PAR} -display sdl"
 PAR="${PAR} -vga none"
-PAR="${PAR} -display none"
+#PAR="${PAR} -display none"
 # NOTE: Guest OS (ubuntu) will not write to virtual and physical display card at once ?!
+# TODO: Dual gpu system?!
+#  https://devtalk.nvidia.com/default/topic/1030445/cuda-setup-and-installation/dual-gpu-system-in-ubuntu-16-04/
 
+# Display 'spice'
+SPICE_PORT=5924
+if [[ -n ${SPICE_PORT} ]]; then
+	# https://wiki.gentoo.org/wiki/QEMU/Linux_guest
+	# https://www.spice-space.org/download.html
+	# $ sudo apt-get install spice-vdagent
+	# -> This will add bidirectonal clipboard among other stuff ;)
+	PAR="${PAR} -spice port=${SPICE_PORT},disable-ticketing"
+	# 
+	PAR="${PAR} -device virtio-serial"
+	PAR="${PAR} -chardev spicevmc,id=vdagent,name=vdagent"
+	PAR="${PAR} -device virtserialport,chardev=vdagent,name=com.redhat.spice.0"
+fi
 
 # Monitoring screen
 PAR="${PAR} -monitor stdio"
 
+
+# USB redirection
+USB_REDIR=true
+USB_REDIR_TYPE="USB3"
+#USB_REDIR_TYPE="USB2"
+if [[ -n ${USB_REDIR} ]]; then
+	# https://www.spice-space.org/usbredir.html
+	# NOTE: ... hmmm ... this works wihout installing anything.
+	#       Maybe things were alredy installed or was installed with 'spice-vdagent'
+	case "${USB_REDIR_TYPE}" in
+		"USB3" )
+			# NOTE: USB3 support
+			PAR="${PAR} -device nec-usb-xhci,id=usb"
+			;;
+		"USB2" )
+			# NOTE: USB2 support
+			PAR="${PAR} -device ich9-usb-ehci1,id=usb"
+			PAR="${PAR} -device ich9-usb-uhci1,masterbus=usb.0,firstport=0,multifunction=on"
+			PAR="${PAR} -device ich9-usb-uhci2,masterbus=usb.0,firstport=2"
+			PAR="${PAR} -device ich9-usb-uhci3,masterbus=usb.0,firstport=4"
+			;;
+		"FAILED" | * )
+			lm_failure ;;
+	esac
+	
+	PAR="${PAR} -chardev spicevmc,name=usbredir,id=usbredirchardev1"
+	PAR="${PAR} -device usb-redir,chardev=usbredirchardev1,id=usbredirdev1"
+	PAR="${PAR} -chardev spicevmc,name=usbredir,id=usbredirchardev2"
+	PAR="${PAR} -device usb-redir,chardev=usbredirchardev2,id=usbredirdev2"
+	PAR="${PAR} -chardev spicevmc,name=usbredir,id=usbredirchardev3"
+	PAR="${PAR} -device usb-redir,chardev=usbredirchardev3,id=usbredirdev3"
+	
+	# Filtering ?
+	#PAR="${PAR} -device usb-redir,filter='0x03:-1:-1:-1:0|-1:-1:-1:-1:1',chardev=usbredirchardev1,id=usbredirdev1"
+fi
+
 # USB passthrough. Keyboard and mouse.
 # TODO: parameterize. Or auto find.
-PAR="${PAR} -usb -usbdevice host:046d:c077"
-PAR="${PAR} -device usb-host,hostbus=1,hostaddr=4"
-#PAR="${PAR} -usbdevice tablet"
-
+PAR="${PAR} -usb -usbdevice host:046d:c077" # Bus 001 Device 006: ID 046d:c077 Logitech, Inc. M105 Optical Mouse
+PAR="${PAR} -device usb-host,hostbus=1,hostaddr=4" # Bus 001 Device 007: ID 046d:c31c Logitech, Inc. Keyboard K120
+PAR="${PAR} -usbdevice tablet"
 
 # OVMF
 PAR="${PAR} -drive file=${OVMF_CODE},if=pflash,format=raw,unit=0,readonly=on"
@@ -432,6 +514,11 @@ PAR="${PAR} -drive file=${OVMF_VARS_UBUNTU},if=pflash,format=raw,unit=1"
 PAR="${PAR} -device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1"
 
 # VGA passthrough. GPU and sound.
+# TODO: Ask user which card should be used.
+#NVIDIA_GPU="01:00.0"
+#NVIDIA_SOUND="01:00.1"
+#NVIDIA_GPU="02:00.0"
+#NVIDIA_SOUND="02:00.1"
 if [[ ! -z ${NVIDIA_GPU} ]]; then
 	PAR="${PAR} -device vfio-pci,host=${NVIDIA_GPU},bus=root.1,addr=00.0,multifunction=on,x-vga=on"
 fi
@@ -453,6 +540,24 @@ fi
 # Virtual disk
 PAR="${PAR} -drive file=${VM_DISK_UBUNTU},format=qcow2,if=none,id=drive-ide0-0-0"
 PAR="${PAR} -device ide-hd,bus=ide.0,unit=0,drive=drive-ide0-0-0,id=ide0-0-0"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Sound card
@@ -496,7 +601,6 @@ PAR="${PAR} -netdev bridge,br=virbr0,id=user.0"
 PAR="${PAR} -device e1000,netdev=user.0"
 
 
-
 # Start the virtual machine with parameters
 echo ""
 echo "qemu-system-x86_64 ${PAR}"
@@ -505,10 +609,18 @@ echo "https://en.wikibooks.org/wiki/QEMU/Monitor"
 echo " (qemu) help"
 echo " (qemu) info pci"
 echo ""
+
+if [[ -n ${SPICE_PORT} ]]; then
+	echo ""
+	echo "Connect to 'spice' remote server."
+	echo " $ spicy --title Windows 127.0.0.1 -p ${SPICE_PORT}"
+	echo "You could also use 'remote-viewer'"
+	echo " $ remote-viewer --title Windows spice://127.0.0.1:${SPICE_PORT}"
+fi
+
+echo ""
 #qemu-system-x86_64 ${PAR}
 sudo qemu-system-x86_64 ${PAR}
-
-#echo "qemu-system-x86_64 ${PAR}"
 
 
 echo ""
