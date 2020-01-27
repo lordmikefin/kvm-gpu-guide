7551c26183277ce85850f1fec1269c2e8ccec4d3
Lord Mike <lordmike@iki.fi>
2020-01-27 15:13:29 +0200

    Can not staticly connect phone USB :(

--- guest/windows/windows_10_clean.sh
+++ guest/windows/windows_10_clean.sh
@@ -505,6 +505,8 @@ fi
 # TODO: parameterize. Or auto find.
 PAR="${PAR} -usb -usbdevice host:046d:c077" # Bus 001 Device 006: ID 046d:c077 Logitech, Inc. M105 Optical Mouse
 #PAR="${PAR} -device usb-host,hostbus=1,hostaddr=4" # Bus 001 Device 007: ID 046d:c31c Logitech, Inc. Keyboard K120
+#PAR="${PAR} -usb -usbdevice host:0e8d:2008" # Bus 001 Device 005: ID 0e8d:2008 MediaTek Inc. (BV6000 Transfer files)
+#PAR="${PAR} -usb -usbdevice host:0e8d:200b" # Bus 001 Device 009: ID 0e8d:200b MediaTek Inc. (BV6000 Transfer photos)
 PAR="${PAR} -usbdevice tablet"
 
 # OVMF
