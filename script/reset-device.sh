4fb1882b3d519a4cde0867ec775a49a4bcba0743
Lord Mike <lordmike@iki.fi>
2020-11-16 17:14:41 +0200

    created reset device script

---
+++ script/reset-device.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# https://forum.proxmox.com/threads/vfio-pci-refused-to-change-power-state-currently-in-d3.60846/
+
+# Usage:
+#  $ sudo ./reset-device.sh 01:00
+
+#echo 0000:${1}
+
+echo "disconnecting amd graphics"
+echo "1" | tee -a /sys/bus/pci/devices/0000:${1}.0/remove
+echo "disconnecting amd sound counterpart"
+echo "1" | tee -a /sys/bus/pci/devices/0000:${1}.1/remove
+#echo "entered suspended state press power button to continue"
+#echo -n mem > /sys/power/state
+echo "reconnecting amd gpu and sound counterpart"
+echo "1" | tee -a /sys/bus/pci/rescan
+echo "AMD graphics card sucessfully reset"
+
+
