fb60dcdb77261b2e60e8144bd87bfff2dc017dea
Lord Mike <lordmike@iki.fi>
2021-01-10 15:32:43 +0200

    Created new wallet

--- guest/linux/ethminer_for_ever.sh
+++ guest/linux/ethminer_for_ever.sh
@@ -6,6 +6,9 @@
 
 while true
 do
-  ethminer --opencl --pool stratum://0x7c5ceff4c98551570c0dca4cd09d9e37e95aa01c@eu1.ethermine.org:4444
+  # Coinmotion address
+  #ethminer --opencl --pool stratum://0x7c5ceff4c98551570c0dca4cd09d9e37e95aa01c@eu1.ethermine.org:4444
+  # Binance address
+  ethminer --opencl --pool stratum://0xbb1c6750ce9f76e6f53540cb37155ef7ff3354ab@eu1.ethermine.org:4444
   sleep 1
 done
